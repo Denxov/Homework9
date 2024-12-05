fa394a219f7cafaf6dc6688e79eb551efe4a083c
Denxtov <denxtov@bk.ru>
2024-12-05 15:38:07 +0300

    Initial

--- module_9_1.py
+++ module_9_1.py
@@ -1,7 +1,10 @@
+
 def apply_all_func(int_list,*functions):
     res={}
     for i in functions:
-        for ii in int_list:
+        res[i.__name__]=(i(int_list))
+    return res
 
-            res[i]
-            pass
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))

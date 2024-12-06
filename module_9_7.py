290277086b48eeced6846f54efe212d4cb5901bc
Denxtov <denxtov@bk.ru>
2024-12-07 00:58:10 +0300

    Amend 0.1 W float support

---
+++ module_9_7.py
@@ -0,0 +1,21 @@
+
+
+def is_prime(func):
+    def wrapper(*args):
+        result=func(*args)
+        for i in range(2,result-1):
+            if result%i==0:
+                print('Составное')
+                return result
+        print('Простое')
+        return result
+    return wrapper
+
+@is_prime
+def sum_three(n1,n2,n3):
+    return n1+n2+n3
+
+result = sum_three(2, 3, 6)
+print(result)
+
+
